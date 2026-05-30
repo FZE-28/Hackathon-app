db6d1b6a922d6c771a477ce92b5e56eafc809a22
FZE-28 <fatima.z.e-schule@outlook.de>
2026-05-30 10:19:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import chromadb
+
+@st.cache_resource
+def lade_datenbank():
+    client = chromadb.PersistentClient(path="./ki_speicher")
+    collection = client.get_or_create_collection(name="produktentwicklung")
+    return collection
+  
+db = lade_datenbank()
+
+
+
+
 st.title("Product development")
 st.write("Ask your question.")
 user_frage = st.text_input("Enter your prompt here")
